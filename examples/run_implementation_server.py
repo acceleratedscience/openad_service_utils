645fe9758e7a819985fda52a8351f2a94a2ccc3c
Brian Duenas <brian.duenas@ibm.com>
2024-07-10 23:48:37 +0000

    comments

--- examples/run_implementation_server.py
+++ examples/run_implementation_server.py
@@ -1,6 +1,8 @@
 
 
 if __name__ == "__main__":
+    # first import the model to load into registry
     from template_implementation import MyModelGenerator
+    # start the server
     from openad_service_utils.api.server import start_server
     start_server(port=8090)
