66a726e531131278c8f250b877363fa9e70371bd
Cosmic Syntax <choi.mina@gmail.com>
2025-04-10 21:53:45 -0400

    debuggin request

--- src/openad_service_utils/api/generation/call_generation_services.py
+++ src/openad_service_utils/api/generation/call_generation_services.py
@@ -122,8 +122,10 @@ class service_requester:
         current_service = None
         for service in get_services():
             logger.warning("service type\n\n\n" + str(type(service)))
-
             logger.warning("service \n\n\n" + str(service))
+
+            logger.warning("request type\n\n\n" + str(type(request)))
+            logger.warning("request \n\n\n" + str(request))
             if (
                 service["service_type"] == request["service_type"]
                 and service["service_name"] == request["service_name"]
